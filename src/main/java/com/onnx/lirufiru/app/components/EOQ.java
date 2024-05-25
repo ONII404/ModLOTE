3c0333582669df09c6c7b172725cc3edb6443016
ONII <beehrtbeehrt@gmail.com>
2024-05-25 05:30:42 -0500

    EOQ Completo
    Finalización del EOQ o compra, cuando Q es constante y cuando es volátil.

--- src/main/java/com/onnx/lirufiru/app/components/EOQ.java
+++ src/main/java/com/onnx/lirufiru/app/components/EOQ.java
@@ -13,27 +13,30 @@ public class EOQ {
      * D = Demanda Anual de unidades
      * S = Costo por hacer pedidos
      * N = Numero de pedidos al año
-     * t = Tiempo de ciclo/entrega
+     * t = Tiempo de ciclo en dias
+     * L = Tiempo de Entrega en Dias
      * 
-     * L = Tiempo de Espera
      * R = Punto de reorden
+     * 
+     * Sigma = Desviacion estandar de la demanda
+     * Z = Nivel de servicio
+     * B = Factor de seguridad
+     * SigmaL = Desviacion estandar del tiempo de entrega
      * 
      * 
      */
 
-    public double C, Q, S;
+    public double C, Q, S, R, L, n, Z, B, Sigma, SigmaL;
     // Variables Diarias
     public double d, h;
     // Variables Anuales
     public double H, D, N, t;
 
-    //
-    double q, L, T, n, z, l, pr, sig, sigL;
-
-    // Constructor EOQ sin Q
-    public EOQ(double D, int tD, double S, double H, int tH) {
+    // Constructor EOQ con demanda constante
+    public EOQ(double D, int tD, double S, double H, int tH, double L) {
 
         this.S = S;
+        this.L = L;
 
         // Seteo de D y d
         switch (tD) {
@@ -69,6 +72,39 @@ public class EOQ {
         // Seteo de N
         getN();
 
+        // Seteo de R
+        getR();
+
+        getCT();
+
+    }
+
+    // Constructor EOQ con demanda Variable
+    public EOQ(double Q, double L, double Z, double Sigma, double t) {
+
+        this.Q = Q;
+        this.L = L;
+        this.Z = Z;
+        this.t = t;
+        this.Sigma = Sigma;
+
+
+        getn();
+        getSigmaL();
+        getSS();
+
+        this.Q =+ B;
+
+        // // Seteo de N
+        // getN();
+        // // Seteo de R
+        // getR();
+
+        // getCT();
+
+
+
+
     }
 
     /***
@@ -90,12 +126,41 @@ public class EOQ {
         this.N = D / Q;
     }
 
+    // Calculo del tiempo de ciclo
     void getT() {
         this.t = Q / d;
     }
 
-    // Calculo del Coste total
-    double getCT(double Q, double D, double S, double H) {
-        return (S * D / Q) + (H * Q / 2);
+    // Calculo de n
+    void getn() {
+        this.n = L / t;
     }
+
+    // Calculo del punto de reorden
+    void getR() {
+        this.R = d * L;
+    }
+
+    // Calculo del Coste total
+    void getCT() {
+        this.C = (S * D / Q) + (H * Q / 2);
+    }
+
+    // Calculo de SigmaL
+    void getSigmaL() {
+
+        if (L > t) {
+            this.SigmaL = Math.sqrt((L - (n * t)) * Math.pow(Sigma, B));
+        } else {
+            this.SigmaL = Math.sqrt(L * Math.pow(Sigma, 2));
+        }
+    }
+
+    // Calculo del Stock de Seguridad
+    void getSS() {
+
+        this.B = Z * SigmaL;
+
+    }
+
 }
--- src/main/java/com/onnx/lirufiru/app/main.java
+++ src/main/java/com/onnx/lirufiru/app/main.java
@@ -1,17 +1,14 @@
 package com.onnx.lirufiru.app;
 
 import com.formdev.flatlaf.FlatDarkLaf;
-import com.onnx.lirufiru.app.components.EOQ;
 import com.onnx.lirufiru.app.components.GUI;
-
 
 public class main {
 
     public static void main(String[] args) {
-        
+
         FlatDarkLaf.setup();
         createAndShowGUI();
-        test();
 
     }
 
@@ -19,34 +16,6 @@ public class main {
 
         GUI gui = new GUI();
         gui.setVisible(true);
-
-        System.out.println("GUI Creada");
-
-    }
-
-
-
-
-    /**
-     * tests
-     */
-
-    public static void test() {
-        EOQ eoq = new EOQ(30.0, 1, 100.0, 0.02, 1);
-
-        System.out.println("Demanda Anual: " + eoq.D);
-        System.out.println("Demanda Diaria: " + eoq.d);
-
-        System.out.println("Costo de Mantenimiento Anual: " + eoq.H);
-        System.out.println("Costo de Mantenimiento Diario: " + eoq.h);
-
-        System.out.println("Costo de Hacer Pedidos: " + eoq.S);
-
-        System.out.println("Cantidad Optima de Pedido: " + eoq.Q);
-
-        System.out.println("Numero de Pedidos Anual: " + eoq.N);
-
-        System.out.println("Tiempo de Ciclo: " + eoq.t);
 
     }
 
