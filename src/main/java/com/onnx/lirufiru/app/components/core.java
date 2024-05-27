b8f314d74f178323ac558e17e33437d990468a56
ONII <beehrtbeehrt@gmail.com>
2024-05-27 04:01:55 -0500

    Completado de ELS con GUI

--- src/main/java/com/onnx/lirufiru/app/components/core.java
+++
@@ -1,8 +0,0 @@
-package com.onnx.lirufiru.app.components;
-
-public class core {
-    
-
-    // Aqui se definen las Funciones a utilizar en el programa para rezolver los diferentes metodos
-    
-}
